d978e1e6f17b8e0cca9eada750f35b05e9cd2bf2
Wiktor Słodkowski <2473954@teb-gdansk.ad>
2025-09-30 13:58:24 +0200

    Initial

---
+++ Memory_wfa/Linear2DArray.cs
@@ -0,0 +1,47 @@
+﻿namespace Memory_wfa;
+
+public class Linear2DArray<T>
+{
+    private readonly T[] _data;
+    private readonly int _width;
+    private readonly int _height;
+    
+    public int Width => _width;
+    public int Height => _height;
+
+    public Linear2DArray(int width, int height)
+    {
+        if (width <= 0 || height <= 0) throw new ArgumentOutOfRangeException("Numbers must be greater than zero");
+        
+        _width = width;
+        _height = height;
+        _data = new T[width * height];
+    }
+
+    public T this[int x, int y]
+    {
+        get
+        {
+            ValidationConstraints(x, y);
+            return _data[x * _height + y];
+        }
+        set
+        {
+            ValidationConstraints(x, y);
+            _data[x * _height + y] = value;
+        }
+    }
+
+    private void ValidationConstraints(int x, int y)
+    {
+        if (x < 0 || x >= _width || y < 0 || y >= _height) throw new ArgumentOutOfRangeException("Invalid coordinates");
+    }
+
+    public void ApplyList(IEnumerable<T> list)
+    {
+        if (!(list.Count() == _width * _height))
+        {
+            
+        }
+    }
+}
---
+++ Memory_wfa/Memory.cs
@@ -0,0 +1,70 @@
+﻿namespace Memory_wfa;
+
+public class Memory
+{
+    public static (string, string)[] CardList = new[]
+    {
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+        ("Name", "ImageData"),
+    };
+    
+    Linear2DArray<Card> CardArray = new Linear2DArray<Card>(4, 4);
+    public Memory() {}
+
+    public void GenerateNew()
+    {
+        List<Card> cardList = new List<Card>();
+
+        foreach (var cardDetails in CardList)
+        {
+            cardList.Add(Card.Build(cardDetails.Item1, cardDetails.Item2));
+            cardList.Add(Card.Build(cardDetails.Item1, cardDetails.Item2));
+        }
+
+        Random rng = new Random();
+
+        {
+            int n = cardList.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = cardList[k];
+                cardList[k] = cardList[n];
+                cardList[n] = value;
+            }
+        }
+
+        for (int i = 0; i < cardList.Count; i++)
+        {
+            int x = i / CardArray.Width;
+            int y = i  % CardArray.Width;
+            CardArray[x,y] = cardList[i];
+        }
+    }
+}
+
+public class Card
+{
+    public bool IsDiscovered { get; set; }
+    public string Name { get; set; }
+    public byte[] Image { get; set; }
+
+    private Card() {}
+    
+    public static Card Build(string Name, string ImageRef)
+    {
+        return new Card()
+        {
+            IsDiscovered = false,
+            Name = Name,
+            Image = [], // ImageRef TODO
+        };
+    }
+}
